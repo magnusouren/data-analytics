6ccf35e74fbb7938e60c91b852860ca1496413b7
magnusouren <magnut0203@gmail.com>
2025-04-03 17:06:48 +0200

    update styling of scatterplot

--- scatterplots.py
+++ scatterplots.py
@@ -23,7 +23,16 @@ sns.set(style="whitegrid")
 # Create scatter plots with linear trend lines
 for i, feature in enumerate(features, 1):
     plt.subplot(2, 3, i)
-    sns.regplot(data=df, x=feature, y="Score", scatter_kws={'alpha': 0.6})
+    plt.xlim(-3, 3)
+    plt.ylim(0, 8)
+    sns.regplot(data=df,
+                x=feature,
+                y="Score",
+                scatter_kws={'alpha': 0.4},
+                line_kws={'color': 'black', 'alpha': 0.7},
+                ci=None,
+                truncate=False
+                )
     plt.title(f"{feature} vs Happiness Score")
     plt.xlabel(feature)
     plt.ylabel("Happiness Score")
